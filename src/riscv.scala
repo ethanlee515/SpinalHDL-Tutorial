901cc7beb901cad55d69a8f41e8d078cb130e7b1
Yi Lee <ylee1228@umd.edu>
2024-12-25 02:22:05 +0000

    Clean up tokenizer a bit

--- src/riscv.scala
+++ src/riscv.scala
@@ -16,44 +16,56 @@ case class Token(content: String) {
   }
 }
 
-object RiscV {
-  def tokenize(program : String) : List[Token] = {
-    val operators = List('(', ')', ',', ':')
-    var tokens = List[Token]()
-    var s = ""
-    for(c <- program) {
-      if(c.isWhitespace) {
-        if(s != "") {
-          tokens = tokens :+ Token(s)
-          s = ""
-        }
-      } else if (c.isLetter) {
-        if(s != "" && s(0).isDigit) {
-          tokens = tokens :+ Token(s)
-          s = ""
-        }
-        s += c
-      } else if (c.isDigit) {
-        s += c
-      } else if (operators.contains(c)) {
-        if(s != "") {
-          tokens = tokens :+ Token(s)
-          s = ""
-        }
-        tokens = tokens :+ Token(c.toString)
-      } else {
-        throw new RuntimeException("tokenizer input is nonsense?")
-      }
-    }
-    if(s != "") {
+class Instruction
+class Add(val rd : Integer, val rs1 : Integer, val rs2: Integer) extends Instruction
+class Addi(val rd : Integer, val rs1 : Integer, val imm: Integer) extends Instruction
+class Sub(val rd : Integer, val rs1 : Integer, val rs2 : Integer) extends Instruction
+class Lw(val rd : Integer, val offset : Integer, val rs1 : Integer) extends Instruction
+class Sw(val rs2 : Integer, val offset : Integer, val rs1 : Integer) extends Instruction
+class Beq(val rs1 : Integer, val rs2 : Integer, val offset : Integer) extends Instruction
+
+class Tokenizer(program : String) {
+  val operators = List('(', ')', ',', ':')
+  var tokens = List[Token]()
+  var s = ""
+
+  def pushToken() = {
       tokens = tokens :+ Token(s)
+      s = ""
+  }
+
+  for(c <- program) {
+    if(c.isWhitespace) {
+      if(s != "") {
+        pushToken()
+      }
+    } else if (c.isLetter) {
+      if(s != "" && s(0).isDigit) {
+        pushToken()
+      }
+      s += c
+    } else if (c.isDigit) {
+      s += c
+    } else if (operators.contains(c)) {
+      if(s != "") {
+        pushToken()
+      }
+      tokens = tokens :+ Token(c.toString)
+    } else {
+      throw new RuntimeException("Tokenizer input contains weird characters?")
     }
-    return tokens
+  }
+  if(s != "") {
+    pushToken()
   }
 }
 
+class Parser(tokens: List[Token]) {
+
+}
+
 object Main extends App {
-  val tokens = RiscV.tokenize("hello world ra(3, 5) label:")
-  println(s"tokens = $tokens")
+  val tokenizer = new Tokenizer("hello world ra(3, 5) label:")
+  println(s"tokens = ${tokenizer.tokens}")
 
 }
