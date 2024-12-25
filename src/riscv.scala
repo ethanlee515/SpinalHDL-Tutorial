66c796d05340c95193e8feea6328b35b68f5d0c5
Yi Lee <ylee1228@umd.edu>
2024-12-25 03:00:09 +0000

    Scrap label support in tokenizer
    Disallow non-keyword identifiers as well

--- src/riscv.scala
+++ src/riscv.scala
@@ -1,20 +1,6 @@
 package riscv
 
-case class Token(content: String) {
-  val keywords = List[String]("add", "addi", "(", ")", ",", ":")
-
-  def isKeyword : Boolean = {
-    return keywords.contains(content) 
-  }
-
-  def isIdentifier : Boolean = {
-    return (!isKeyword) && content(0).isLetter
-  }
-
-  def isNumber : Boolean = {
-    return content(0).isDigit
-  }
-}
+case class Token(content: String)
 
 class Instruction
 class Add(val rd : Integer, val rs1 : Integer, val rs2: Integer) extends Instruction
@@ -25,13 +11,24 @@ class Sw(val rs2 : Integer, val offset : Integer, val rs1 : Integer) extends Ins
 class Beq(val rs1 : Integer, val rs2 : Integer, val offset : Integer) extends Instruction
 
 class Tokenizer(program : String) {
-  val operators = List('(', ')', ',', ':')
+  val instructions = List("add", "addi", "sub", "lw", "sw", "beq")
+  val register_names = List("zero", "ra", "sp", "gp", "tp",
+    "t0", "t1", "t2", "fp", "s1",
+    "a0", "a1","a2", "a3", "a4", "a5", "a6", "a7",
+    "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9", "s10", "s11",
+    "t3", "t4", "t5", "t6")
+  val operators = List("(", ")", ",", "-")
+
   var tokens = List[Token]()
-  var s = ""
+  private var s = ""
 
   def pushToken() = {
-      tokens = tokens :+ Token(s)
-      s = ""
+    val allowed_tokens = instructions ++ operators ++ register_names
+    if ((!allowed_tokens.contains(s)) && (!s.matches("^\\d+$"))) {
+      throw new RuntimeException(s"Bad token: ${s}")
+    }
+    tokens = tokens :+ Token(s)
+    s = ""
   }
 
   for(c <- program) {
@@ -46,13 +43,13 @@ class Tokenizer(program : String) {
       s += c
     } else if (c.isDigit) {
       s += c
-    } else if (operators.contains(c)) {
+    } else if (operators.contains(c.toString)) {
       if(s != "") {
         pushToken()
       }
       tokens = tokens :+ Token(c.toString)
     } else {
-      throw new RuntimeException("Tokenizer input contains weird characters?")
+      throw new RuntimeException(s"Tokenizer input contains weird character: $c")
     }
   }
   if(s != "") {
@@ -61,11 +58,23 @@ class Tokenizer(program : String) {
 }
 
 class Parser(tokens: List[Token]) {
+  var loc = 0
 
+  def match_next() = {
+    val token = tokens(loc)
+    token.content match {
+      case "addi" => {
+        println("addi")
+      }
+      case _ => {
+        println("default")
+      }
+    }
+  }
 }
 
 object Main extends App {
-  val tokenizer = new Tokenizer("hello world ra(3, 5) label:")
-  println(s"tokens = ${tokenizer.tokens}")
+  val tokenizer = new Tokenizer("addi ra(3, 5) lw s1 s5, ra")
+  val parser = new Parser(tokenizer.tokens)
 
 }
